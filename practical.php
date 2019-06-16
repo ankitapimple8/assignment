9228826d5b5b0ef885da3b94d771a45bbc9234b3
ANKITA PIMPLE <51016612+ankitast@users.noreply.github.com>
2019-06-16 14:59:46 +0530

    Add files via upload

---
+++ practical.php
@@ -0,0 +1,76 @@
+<aside class="left-sidebar">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- User profile -->
+                
+                <!-- End User profile text-->
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                        <h3 class="nav-small-cap">Dashboard</h3>
+                        <li>
+                            <a class="has-arrow waves-effect waves-dark" target="schoolprofile.php" aria-expanded="false"><i class="mdi mdi-school"></i><span class="hide-menu">School </span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="school.php">View/Add</a></li>
+                                <li><a href="#">Add</a></li>
+                                <li><a href="#">Moderate</a></li>
+                                <li><a href="#">Support</a></li>
+                                <li><a href="board.php">Boards</a></li>
+                                <li><a href="facilities.php">Facilities</a></li>
+                                
+                            </ul>
+                        </li>
+                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-human-male-female"></i><span class="hide-menu">Parent</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="parent.php">View/Edit</a></li>
+                            <li><a href="#">Moderations</a></li>
+                            </ul>
+
+                        </li>
+                        <li>
+                            <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-human-child"></i><span class="hide-menu">Child</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="child.php">View/Edit</a></li>
+                                <li><a href="app-ticket.html">Moderations</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="fa fa-support"></i><span class="hide-menu">Support</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="#">School</a></li>
+                                <li><a href="#">Parent</a></li>
+                                <li><a href="#">Child</a></li>
+                            </ul>
+                        </li> 
+                        <li>
+                            <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-help-circle-outline"></i><span class="hide-menu">FAQ</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="viewfaq.php">View/Edit</a></li>
+                                <li><a href="#">Add</a></li>
+                            </ul>
+                        </li> 
+                <li>
+                            <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-human-greeting"></i><span class="hide-menu">Team Members</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="team.php">View/Edit</a></li>
+                                <li><a href="#">Add</a></li>
+                                
+                            </ul>
+                        </li> 
+                </nav>
+
+
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+            <!-- Bottom points-->
+            <div class="sidebar-footer">
+                <!-- item-->
+                <a href="" class="link" data-toggle="tooltip" title="Settings"><i class="ti-settings"></i></a>
+                <!-- item-->
+                <a href="" class="link" data-toggle="tooltip" title="Email"><i class="mdi mdi-gmail"></i></a>
+                <!-- item-->
+                <a href="" class="link" data-toggle="tooltip" title="Logout"><i class="mdi mdi-power"></i></a>
+            </div>
+            <!-- End Bottom points-->
+        </aside>
